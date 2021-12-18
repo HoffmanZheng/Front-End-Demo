4d71156b70fbd65ae0b2809215a0345f96d6b956
Chenghao Zheng <zch693922442@outlook.com>
2021-12-18 16:20:47 +0800

    finish component life cycle, init react-staging

---
+++ react_staging/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  // 和 ES5 严格模式没有关系，只是检查 App 以及它的子组件是否合理
+  // 比如字符串 ref 会检查并报警
+  <React.StrictMode>  
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
