3ed68d98969dae53491a2e502deb4e1d87e7ce81
Chenghao Zheng <zch693922442@outlook.com>
2021-12-26 23:10:07 +0800

    redirect

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class App extends Component {
+    getData = () => {
+        axios.get('http://www.baidu.com').then(
+            response => {
+                console.log('successfully')
+            },
+            error => {
+                console.log("error")
+            }
+        )
+    }
+
     render() {
         return (
-            <div>hello</div>
+            <div>
+                <h3>hello</h3>
+                <button onClick={this.getData}>点我获取数据</button>
+            </div>
+            
         );
     }
 }
