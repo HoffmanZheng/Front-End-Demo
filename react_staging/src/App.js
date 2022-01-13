6d8b05682d9aa4135d508b504dd93fc40fc82de0
Chenghao Zheng <zch693922442@outlook.com>
2022-01-14 00:19:43 +0800

    finish router

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
-import Search from './component/search'
-import Display from './component/display'
+import {Link, BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about';
 
 class App extends Component {
 
     render() {
         return (
             <div>
-                <Search />
-                <Display /> 
+                <div className='row'>
+                    <div className='col-xs-offset-2 col-xs-8'>
+                        <div className='page-header'>
+                            <h2>React Router Demo</h2>
+                        </div>
+                    </div>
+                </div>
+                <BrowserRouter>
+                    <div className='row'>
+                        <div className='col-xs-2 col-xs-offset-2'>
+                            <div className='list-group'>
+                                <Link className='list-group-item' to="/home">home</Link>
+                                <Link className='list-group-item' to="/about">about</Link>
+                            </div>
+                        </div>
+                        <div className='col-xs-6'>
+                            <div className='panel'>
+                                <div className='panel-body'>
+                                    <Routes>
+                                        <Route path="/home" element={<Home/>} />
+                                        <Route path="/about" element={<About/>} />
+                                    </Routes>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </BrowserRouter>
             </div>
         );
     }
