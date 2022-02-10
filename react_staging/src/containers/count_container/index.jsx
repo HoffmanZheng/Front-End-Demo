ca3d6f92b1e78bb303324f4f8520a1113d420fb0
Hoffman Zheng <hoffman.zheng@goat.com>
2022-02-10 18:27:45 +0800

    sync

--- react_staging/src/containers/count_container/index.jsx
+++ react_staging/src/containers/count_container/index.jsx
@@ -4,16 +4,19 @@ import {createIncrementAction, createDecrementAction, createAynsIncrementAction}
 
 
 // mapStateToProps 函数就是把状态传递给 UI 组件
-function mapStateToProps(state) {
-    return {sum: state}
-}
-
 // mapDispatchToProps 函数用于传递操作状态的方法
-function mapDispatchToProps(dispatch) {
-    return {add: num => dispatch(createIncrementAction(num)), 
-    subtract: num => dispatch(createDecrementAction(num)),
-    asyncAdd: (num, time) => dispatch(createAynsIncrementAction(num, time))}
-}
 
-const CountContainer = connect(mapStateToProps, mapDispatchToProps)(Count)
+const CountContainer = connect(
+    state => ({sum: state}), 
+    // dispatch => ({
+    //     add: num => dispatch(createIncrementAction(num)), 
+    //     subtract: num => dispatch(createDecrementAction(num)),
+    //     asyncAdd: (num, time) => dispatch(createAynsIncrementAction(num, time))})
+    // )
+    {
+        add: createIncrementAction,
+        subtract: createDecrementAction,
+        asyncAdd: createAynsIncrementAction
+    }
+)(Count)
 export default CountContainer
