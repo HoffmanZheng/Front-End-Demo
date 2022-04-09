d8e6730a852263a90dfd562d1d74697d6cea4453
Hoffman Zheng <hoffman.zheng@goat.com>
2022-04-09 16:23:20 +0800

    static server: fetch file content according to the url

--- node.js_basic/static_web_server/index.ts
+++ node.js_basic/static_web_server/index.ts
@@ -1,9 +1,43 @@
 import * as http from 'http';
-console.log(123);
-const server = http.createServer();
+import * as fs from 'fs';
+import path from 'path';
 
-server.on('request', ()=> {
-    console.log('hi')
+const server: http.Server = http.createServer(); 
+const staticPath = path.resolve(__dirname, 'static')
+
+server.on('request', (request, response)=> {
+    const url = request.url
+
+    switch (url) {
+        case '/index.html':
+            response.setHeader('Content-type', 'text/html; charset=utf-8')
+            fs.readFile(path.resolve(staticPath, 'index.html'), (err, data) => {
+                if (err) throw err;
+                response.end(data);
+            })
+            break;
+        case '/style.css':
+            response.setHeader('Content-type', 'text/css; charset=utf-8')
+            fs.readFile(path.resolve(staticPath, 'style.css'), (err, data) => {
+                if (err) throw err;
+                response.end(data);
+            })
+            break;
+        case '/main.js':
+            response.setHeader('Content-type', 'application/x-javascript; charset=utf-8')
+            fs.readFile(path.resolve(staticPath, 'main.js'), (err, data) => {
+                if (err) throw err;
+                response.end(data);
+            })
+            break;
+        default:
+            response.end('welcome')
+    }
+
+
+
 })
 
-server.listen(8888);
+server.listen(8888, () => {
+    console.log(server.address());
+});
---
+++ node.js_basic/static_web_server/static/main.js
@@ -0,0 +1,4 @@
+const h2 = document.querySelector('h2')
+h2.onclick = () => {
+    alert('hi')
+}
